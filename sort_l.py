d1e49cff18e1f3d1a3cb38930ce0b21bd0146f79
AndreiAniukou <antei1231@gmail.com>
2019-10-20 23:46:24 +0300

    sort test

---
+++ sort_l.py
@@ -0,0 +1,22 @@
+cars = ['bmw', 'audi', 'mercedes', 'volvo', 'mybach']
+cars.sort(reverse=True) #memory sort
+print(cars)
+
+cars = ['bmw', 'audi', 'mercedes', 'volvo', 'mybach']
+print('\nOriginal list: ' + str(cars))
+print('\nSorted list: ' + str(sorted(cars)))  #Sorted without memory
+
+print('\nOriginal list: ' + str(cars))
+
+cars = ['bmw', 'audi', 'mercedes', 'volvo', 'mybach']
+
+cars.reverse()#Reverse list
+print('\n' + str(cars))
+cars.reverse()#Reverse list back
+print(cars)
+
+print('\n__________________')
+
+cars = ['bmw', 'audi', 'mercedes', 'volvo', 'mybach']
+
+print(len(cars)) #len - length list
---
+++ sort_l_test.py
@@ -0,0 +1,43 @@
+
+
+travel = ['USA', 'France', 'Egyrt', 'Russia', 'Spain', 'China', 'Argentina', 'Belarus']
+print('\nOriginal list: ' + str(travel))
+
+print('\nSorted list: ' + str(sorted(travel)))
+
+print('\nOriginal: ' + str(travel))
+
+print('\nReverse: ' + str(sorted(travel, reverse=True)))
+
+print('\nOriginal 2: ' + str(travel))
+
+print('\n__________________\n')
+
+travel.reverse()
+print('\nOriginal revers: ' + str(travel))
+
+travel.reverse()
+print('\nOriginal back: ' + str(travel))
+
+travel.sort()
+print('\nOriginal sort: ' + str(travel))
+
+travel.sort(reverse=True)
+print('\nOriginal sort revers: ' + str(travel))
+
+'''FROM change_lsit_test i cope 'name' list'''
+
+name = ['Ellie', 'Arsen', ' Max', 'oleg', 'margo', 'ann', 'victoria', 'andre']
+
+print('\n' + str(len(name)))
+
+'''TEST PROGRAMM WITH ALL FUN LIST'''
+
+print('\n__________________')
+
+car_name = ['audi', 'aston martin', 'bmw', 'mercedes', 'toyota', 'gmc',
+            'maybah', 'vw', 'volvo', 'maserati', 'kia', 'honda', 'nissan']
+print(car_name)
+
+car_name.sort()
+print('\n' + str(car_name))
