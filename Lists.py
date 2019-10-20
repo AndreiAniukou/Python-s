8fab27b0cef40f7658897f012da6aee95fe38beb
AndreiAniukou <antei1231@gmail.com>
2019-10-20 13:40:29 +0300

    1

---
+++ Lists.py
@@ -0,0 +1,27 @@
+bic = ['track', 'speed', 'redline', 'highline']
+print(bic[0].title())
+print(bic[-1])
+
+message = '\n\t My first bicycle was a ' + bic[0].title() + '.'
+print(message)
+
+name = ['Paddy', 'Luqman', 'Domas', 'carty', 'alastair']
+
+print(name[-1].title() + '\n' + name[-2].title() +
+      '\n' + name[-3].title() + '\n' + name[-4].title() +
+      '\n' + name[-5].title())
+
+
+message = '\n' + name[0].title() + ' like playing tennis'
+print(message)
+message = '\n' + name[1].title() + ' like playing tennis'
+print(message)
+message = '\n' + name[2].title() + ' like playing tennis'
+print(message)
+message = '\n' + name[3].title() + ' like playing tennis'
+print(message)
+message = '\n' + name[4].title() + ' like playing tennis'
+print(message)
+
+
+
---
+++ change_list.py
@@ -0,0 +1,17 @@
+moto = ['honda', 'yamaha', 'suzuki']
+print(moto)
+
+moto[0] = 'ducati'
+print(moto)
+
+moto.append('bmw')
+print(moto)
+
+moto = []
+moto.append('bmw')
+moto.append('honda')
+moto.append('suzuki')
+print(moto)
+
+moto.insert(2, 'ducati')
+print(moto)
