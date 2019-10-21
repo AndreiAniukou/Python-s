39f244b7f2be9260c86283396bae016eb65974a4
AndreiAniukou <antei1231@gmail.com>
2019-10-22 00:38:37 +0300

    Range

---
+++ if_for_smt.py
@@ -0,0 +1,17 @@
+magic = ['alice', 'fines', 'david']
+for magics in magic:
+    print(magics)
+
+
+for magics in magic:
+    print('\n' + str(magics.title()) + ', that was a greatetrick')
+    print("i can't wait to see ur next trick, " + magics.title())
+print('\n\tend for')
+
+'''TS for #for# '''
+
+pizza = ['peperoni', 'margarita', '4 season', 'hum and mushrooms']
+
+for pizzas in pizza:
+    print('\nI like ' + pizzas + 'pizza')
+print('i very like pizza!')
---
+++ number_list.py
@@ -0,0 +1,11 @@
+for value in range(0, 10):
+    print(value)
+
+numbers = list(range(0, 10, 2))
+print(numbers)
+
+sqrs = []
+for value in range(0, 10):
+    sqr = value**2
+    sqrs.append(sqr)
+    print(sqrs)
