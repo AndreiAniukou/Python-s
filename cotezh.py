4c23fb602ce4eef07025c17645be55262edd2a67
AndreiAniukou <antei1231@gmail.com>
2019-10-24 19:27:30 +0300

    update tuple

---
+++ cotezh.py
@@ -0,0 +1,16 @@
+demensions = (200 , 50)
+print(demensions[0])
+print(demensions[1])
+
+for dem in demensions:
+    print(dem)
+
+demensions1 = (100, 50)
+print('\nOriginal:')
+for dem in demensions1:
+    print(dem)
+
+print('\nModif:')
+demensions1 = (400, 100)
+for dem in demensions1:
+    print(dem)
---
+++ test_tuple.py
@@ -0,0 +1,11 @@
+menu = ('Potato', 'meat', 'beef', 'hum', 'beer')
+print('All menu:')
+for m in menu:
+    print(m.title())
+
+menu = ('potato', 'salat', 'pepsi', 'sprite')
+print('\nNew menu: ')
+for m in menu:
+    print(m.title())
+
+
